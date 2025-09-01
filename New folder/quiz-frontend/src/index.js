9dc9de9f7ad97a16356351915b3f9ad77be57776
Tseg-tech <tseganehamare@gmail.com>
2025-09-01 16:14:47 +0300

    New folder removed

--- New folder/quiz-frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import QuizApp from './QuizApp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<QuizApp />);
